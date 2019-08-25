606bf9b74d5cabba47170099cd9f3b7f32a16aa3
sdp <403213485@qq.com>
2019-08-25 20:56:39 +0800

    1

--- src/main/java/com/oocl/cultivation/ParkingBoy.java
+++ src/main/java/com/oocl/cultivation/ParkingBoy.java
@@ -11,12 +11,26 @@ public class ParkingBoy {
 
     public ParkingTicket park(Car car) {
         // TODO: Please implement the method
-        throw new RuntimeException("Not implemented");
+    	if(parkingLot.getAvailableParkingPosition() == 0) {
+    		this.lastErrorMessage = "The parking lot is full.";
+    		return null;
+    	}
+    	this.lastErrorMessage = null;
+        return parkingLot.parkCar(car);
     }
 
     public Car fetch(ParkingTicket ticket) {
         // TODO: Please implement the method
-        throw new RuntimeException("Not implemented");
+    	if(ticket == null) {
+    		this.lastErrorMessage = "Please provide your parking ticket.";
+    		return null;
+    	}
+    	Car car = parkingLot.getCarByTicket(ticket);
+    	if(car == null) {
+    		this.lastErrorMessage = "Unrecognized parking ticket.";
+    		return null;
+    	}
+        return car;
     }
 
     public String getLastErrorMessage() {
--- src/main/java/com/oocl/cultivation/ParkingLot.java
+++ src/main/java/com/oocl/cultivation/ParkingLot.java
@@ -16,6 +16,18 @@ public class ParkingLot {
     }
 
     public int getAvailableParkingPosition() {
-        return cars.size() - capacity;
+        return capacity - cars.size();
     }
+    
+    public ParkingTicket parkCar(Car car) {
+    	ParkingTicket ticket = new ParkingTicket();
+    	cars.put(ticket,car);
+    	return ticket;
+    }
+
+	public Car getCarByTicket(ParkingTicket ticket) {
+		// TODO Auto-generated method stub
+		return cars.remove(ticket);
+	}
+    
 }
